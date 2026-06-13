a00c9a64d6dc9aa599e2d312b5bf69f565e95eea
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-06-13 08:07:15 +0530

    Update about_screen.py

--- gui/screens/about_screen.py
+++ gui/screens/about_screen.py
@@ -1,1 +1,16 @@
+import customtkinter as ctk
 
+
+class AboutScreen(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        ctk.CTkLabel(self, text="About", font=("Arial", 20)).pack(pady=20)
+        ctk.CTkLabel(
+            self,
+            text="YouTube Downloader\n\nDownload Video & Audio easily.\n\nBuilt with Python ",
+            justify="center",
+        ).pack(pady=10)
+        ctk.CTkLabel(
+            self,
+            text="Developer: Shakalen Shaikh \n Follow For More :- github.com/ShakalBhau0001 \n Version: 1.0",
+        ).pack(pady=20)
