1cec5ead76035f1f14949e9757f9a243dca1297d
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-06-13 08:11:15 +0530

    Update main_screen.py

--- gui/screens/main_screen.py
+++ gui/screens/main_screen.py
@@ -1,1 +1,22 @@
+import customtkinter as ctk
 
+
+class MainScreen(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.label = ctk.CTkLabel(self, text="YouTube Downloader", font=("Arial", 20))
+        self.label.pack(pady=30)
+        self.video_btn = ctk.CTkButton(
+            self, text="Download Video", command=self.open_video
+        )
+        self.video_btn.pack(pady=10)
+        self.audio_btn = ctk.CTkButton(
+            self, text="Download Audio", command=self.open_audio
+        )
+        self.audio_btn.pack(pady=10)
+
+    def open_video(self):
+        self.master.master.show_video()
+
+    def open_audio(self):
+        self.master.master.show_audio()
